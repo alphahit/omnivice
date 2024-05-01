e8ea979206844a1668d41dee21bfea0a3eaec241
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-05-01 21:21:11 +0530

    Vector Icon Package Added'

--- src/config/ThemeColors.js
+++ src/config/ThemeColors.js
@@ -8,7 +8,17 @@ const colors = {
     secondary: '#FF6347',
     highlight: '#87CEEB',
     shadow: '#F5F5F5',
-    primaryColor: '#0BCA93'
+    primaryColor: '#0BCA93',
+    teal50: '#E0F2F1',
+    teal100: '#B2DFDB',
+    teal200: '#80CBC4',
+    teal300: '#4DB6AC',
+    tealA100: '#A7FFEB',
+    lightGreen: '#81C784',
+    mint: '#3EB489',
+    springGreen: '#00FF7F',
+    mediumTurquoise: '#48D1CC',
+    cyanProcess: '#00B7EB',
   },
   dark: {
     background: '#333333',
@@ -17,7 +27,17 @@ const colors = {
     secondary: '#FF6347',
     highlight: '#81b0ff',
     shadow: '#222222',
-    primaryColor: '#0BCA93'
+    primaryColor: '#0BCA93',
+    teal700: '#00796B',
+    teal800: '#00695C',
+    teal900: '#004D40',
+    tealA700: '#00BFA5',
+    forestGreen: '#228B22',
+    darkCyan: '#008B8B',
+    turquoise: '#30D5C8',
+    darkTurquoise: '#00CED1',
+    seaGreen: '#2E8B57',
+    viridian: '#40826D',
   },
 };
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -10,54 +10,131 @@ import {
   View,
   TouchableOpacity,
   FlatList,
+  StyleSheet,
+  TextInput,
+  Keyboard,
 } from 'react-native';
 import {useTheme} from '../context/ThemeContext';
 import colors from '../config/ThemeColors';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch, useSelector} from 'react-redux';
 import {setColor} from '../store/slices/colorslice';
-
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 const Home = () => {
   const {isDarkMode, setTheme} = useTheme();
+  const [term, setTerm] = useState('');
   const navigation = useNavigation();
 
   const themeColors = isDarkMode ? colors.dark : colors.light;
-  const colorList = useSelector(state => state.color.value); //reading the state
+  // const colorList = useSelector(state => state.color.value);
   const dispatch = useDispatch();
   const backgroundStyle = {
     backgroundColor: themeColors.background,
     flex: 1,
   };
-  const backgroundStyleWithoutFlex = {
-    backgroundColor: themeColors.background,
+  const handleSearchclose = () => {
+    Keyboard.dismiss();
+    setTerm('');
   };
 
-  const textStyle = {
-    color: themeColors.text,
-    fontSize: 18,
-  };
+  const dynamicStyles = StyleSheet.create({
+    v2: {
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      borderWidth: 2,
+      borderColor: themeColors.teal700, // Now this can access themeColors
+      borderRadius: 8,
+      alignItems: 'center',
+      marginTop: -30,
+      marginHorizontal: 20,
+    },
+  });
+  // const backgroundStyleWithoutFlex = {
+  //   backgroundColor: themeColors.background,
+  // };
 
-  const toggleTheme = () => {
-    console.log('Toggling theme');
-    setTheme(isDarkMode ? 'light' : 'dark');
-  };
-  const renderItem = ({item}) => (
-    <View
-      style={{
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: item,
-        marginBottom: 10,
-      }}>
-      <Text style={{color: themeColors.text, fontSize: 16}}>{item}</Text>
-    </View>
-  );
+  // const textStyle = {
+  //   color: themeColors.text,
+  //   fontSize: 18,
+  // };
+
+  // const toggleTheme = () => {
+  //   console.log('Toggling theme');
+  //   setTheme(isDarkMode ? 'light' : 'dark');
+  // };
+  // const renderItem = ({item}) => (
+  //   <View
+  //     style={{
+  //       height: 50,
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       backgroundColor: item,
+  //       marginBottom: 10,
+  //     }}>
+  //     <Text style={{color: themeColors.text, fontSize: 16}}>{item}</Text>
+  //   </View>
+  // );
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar setBackgroundColor={themeColors.primaryColor} barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={{flex:1, backgroundColor: 'white'}}>
+          <View className='' style={{ height: 100, backgroundColor:themeColors.primaryColor, borderBottomStartRadius: 25, borderBottomEndRadius: 25 }}> 
+          <Text>
+              Hi Prateek!
+            </Text>
+            <Text>
+               Find Your Service
+            </Text>
+          </View>
+          <View style={dynamicStyles.v2}>
+          <TouchableOpacity
+            onPress={() => {
+              term.length > 0 && handleSearchclose();
+            }}>
+            {term.length > 0 ? (
+              <AntDesign
+                name="close"
+                size={25}
+                color={themeColors.teal700}
+                style={{marginLeft: 5}}
+              />
+            ) : (
+              <Feather
+                name="search"
+                size={25}
+                color={themeColors.teal700}
+                style={{marginLeft: 5}}
+              />
+            )}
+          </TouchableOpacity>
+          <TextInput
+            style={{
+              fontSize: 16,
+              backgroundColor: 'white',
+              flex: 1,
+              borderRadius: 8,
+              textAlign: 'left',
+              color: 'black',
+            }}
+            placeholder="Search"
+            placeholderTextColor={'lightgrey'}
+            onChangeText={text => setTerm(text)}
+            value={term}
+            //onPressIn={() => setActivateSearch(true)}
+            onSubmitEditing={Keyboard.dismiss}
+          />
+        </View>
+      </View>
       
-      <View
+   
+      
+      <Button
+        title="Navigate To Settings"
+        onPress={() => navigation.navigate('Settings')}
+        color={isDarkMode ? themeColors.primary : themeColors.secondary}
+      />
+         {/* <View
         style={[backgroundStyleWithoutFlex, {flexDirection: 'row', justifyContent: 'space-between'}]}>
         <Text style={textStyle}>Toggle Theme</Text>
         <Button
@@ -66,13 +143,7 @@ const Home = () => {
           color={isDarkMode ? themeColors.primary : themeColors.secondary}
         />
        
-      </View>
-      
-      <Button
-        title="Navigate To Settings"
-        onPress={() => navigation.navigate('Settings')}
-        color={isDarkMode ? themeColors.primary : themeColors.secondary}
-      />
+      </View> */}
     </SafeAreaView>
   );
 };
